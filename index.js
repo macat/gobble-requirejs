d7d2e10d8476ca2c953c3dea8b7f486a03ba8719
Attila Maczak <attila@maczak.hu>
2015-01-03 18:49:27 -0500

    Remove options.out requirement + default callback

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function requirejs ( inputdir, outputdir, options, callback ) {
 	var path = require( 'path' ), r = require( 'requirejs' );
-
-	if ( !options.out ) {
-		throw new Error( "The gobble-requirejs config must specify an 'out' property, plus one or more of 'name', 'include' or 'modules'." );
-	}
+	if ( typeof callback == 'undefined' ) callback = function() {};
 
 	options.baseUrl = path.join( inputdir, options.baseUrl || '' );
-	options.out = path.join( outputdir, options.out );
+
+	if ( options.out ) {
+		options.out = path.join( outputdir, options.out );
+	}
 
 	r.optimize( options, function () { callback(); }, callback );
 };
